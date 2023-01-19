9fb470b764732930d26aee93d9ac48baccc401d2
Foer <39239388+FoerMaster@users.noreply.github.com>
2023-01-19 20:43:35 +0500

    Update audio_effects.h

--- source/audio_effects.h
+++ source/audio_effects.h
@@ -12,13 +12,20 @@ namespace AudioEffects {
 
 	void BitCrush(uint16_t* sampleBuffer, int samples, float quant, float gainFactor) {
 		for (int i = 0; i < samples; i++) {
-			//Signed shorts range from -32768 to 32767
-			//Let's quantize that a bit
-			float f = (float)sampleBuffer[i];
-			f /= quant;
-			sampleBuffer[i] = (uint16_t)f;
-			sampleBuffer[i] *= quant;
-			sampleBuffer[i] *= gainFactor;
+			// apply a low pass filter to remove high frequency noise
+			sampleBuffer[i] = (sampleBuffer[i] + sampleBuffer[i-1]) / 2;
+			// apply a distortion effect to emulate static noise
+			sampleBuffer[i] = sampleBuffer[i] * 1.5;
+			
+			// apply a high pass filter to remove low frequency noise
+			sampleBuffer[i] = sampleBuffer[i] - (sampleBuffer[i-1] / 2);
+			
+			// limit the amplitude to prevent clipping
+			if (sampleBuffer[i] > 32767) {
+				sampleBuffer[i] = 32767;
+			} else if (sampleBuffer[i] < -32767) {
+				sampleBuffer[i] = -32767;
+			}
 		}
 	}
 
